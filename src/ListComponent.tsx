1f99b72f7d9656ae3e20c87561f22fdd5cb2012c
Yusuf Abdulmuiz (Dev.Abu Ridwan) <yusufabdulmuizolasunkanmi@gmail.com>
2025-05-23 12:54:29 +0100

    Update ListComponent.tsx

--- src/ListComponent.tsx
+++ src/ListComponent.tsx
@@ -1,1 +1,12 @@
+import React from 'react';
+
+const ListComponent = ({ items, renderItem }) => {
+  if (!items.length) {
+    return <p>No items to display.</p>;
+  }
+
+  return <ul>{items.map(renderItem)}</ul>;
+};
+
+export default ListComponent;
 
